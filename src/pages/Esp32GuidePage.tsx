ea3f362fff1e43ff3d2d960b8a7ad0d32efff37d
adhyatmdev0apex-prog <adhyatm.dev0apex@gmail.com>
2026-09-11 16:24:56 +0000

    refactor the library/esp-32 cardse

--- src/pages/Esp32GuidePage.tsx
+++ src/pages/Esp32GuidePage.tsx
@@ -33,7 +33,7 @@ export function Esp32GuidePage({ slug }: { slug: string }) {
       <WorkspaceIframe
         src={file}
         title={slug}
-        className="min-h-[70vh] flex-1 rounded-2xl border border-white/10"
+        className="min-h-0 flex-1 rounded-2xl border border-white/10"
         loadingLabel="Loading guide…"
       />
     </div>
